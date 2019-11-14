330ea3acf4da1bce6a9c70e93d377ca20dce7306
Niclas Binninge <alltjagvill@gmail.com>
2019-11-14 15:39:21 +0100

    Save before deleting stuff

--- app/src/main/java/com/example/inventory/RoomsActivity.java
+++ app/src/main/java/com/example/inventory/RoomsActivity.java
@@ -6,42 +6,36 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Toolbar;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.google.firebase.auth.FirebaseAuth;
 
-public class StartActivity extends AppCompatActivity {
-    FirebaseAuth firebaseAuth;
+public class RoomsActivity extends AppCompatActivity {
 
     BottomNavigationView bottomNavigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_start);
-
-        firebaseAuth = FirebaseAuth.getInstance();
-        bottomNavigationView = findViewById(R.id.bottomNavStart);
-//        bottomNavigationView.setSelectedItemId(R.id.action_rooms);
+        setContentView(R.layout.activity_rooms);
+        bottomNavigationView = findViewById(R.id.bottomNavRooms);
 
 
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-                switch(menuItem.getItemId())
-                {
+
+                switch (menuItem.getItemId()) {
                     case R.id.action_rooms:
-                        Intent rooms = new Intent(StartActivity.this, RoomsActivity.class);
+                        Intent rooms = new Intent(RoomsActivity.this, RoomsActivity.class);
                         startActivity(rooms);
                         break;
 
                     case R.id.action_home:
-                        Intent home = new Intent(StartActivity.this, StartActivity.class);
+                        Intent home = new Intent(RoomsActivity.this, StartActivity.class);
                         startActivity(home);
                         break;
+
                     case R.id.action_settings:
-                        Intent settings = new Intent(StartActivity.this, SettingsActivity.class);
+                        Intent settings = new Intent(RoomsActivity.this, SettingsActivity.class);
                         startActivity(settings);
                         break;
                 }
@@ -49,16 +43,7 @@ public class StartActivity extends AppCompatActivity {
                 return true;
             }
         });
-
     }
 
 
-    public void LogOut(View view)
-    {
-        firebaseAuth.signOut();
-
-        Intent intent = new Intent(this, MainActivity.class);
-
-        startActivity(intent);
-    }
 }
